3c08958d3682838c163e4b4aa40139b27eda3048
orel <orele1995@gmail.com>
2016-07-29 12:03:57 +0300

    add

---
+++ Advanced_Ex04/Primes/Primes/PrimeGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    class PrimeGenerator
+    {
+        public List<int> CulcPrime(int start, int end, int parallelism)
+        {
+            var options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = parallelism;
+
+            if (start < 2) { start = 2; } // 2 is the first prime number
+            List<int> prime_numbers = new List<int>();
+
+            Parallel.For(start, end, options, i =>
+            {
+                if (isPrime(i))
+                    prime_numbers.Add(i);
+            });
+
+            
+            return prime_numbers;
+        }
+
+        private bool isPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
---
+++ Advanced_Ex04/Primes/Primes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var watch = Stopwatch.StartNew();
+            Console.WriteLine(CalcPrimes(1, 500, 1).Count);
+            Console.WriteLine(watch.Elapsed);
+            watch.Restart();
+            Console.WriteLine(CalcPrimes(1, 500, 2).Count);
+            Console.WriteLine(watch.Elapsed);
+            watch.Restart();
+            Console.WriteLine(CalcPrimes(1, 500, 3).Count);
+            Console.WriteLine(watch.Elapsed);
+            watch.Restart();
+            Console.WriteLine(CalcPrimes(1,500,4).Count);
+            Console.WriteLine(watch.Elapsed);
+            watch.Restart();
+        }
+
+        static List<int> CalcPrimes (int start, int end, int parallelism)
+        {
+            // int slice = (end - start) / parallelism; // check 
+          
+            PrimeGenerator pg = new PrimeGenerator();
+           return pg.CulcPrime(start, end, parallelism);
+        }
+
+    }
+}
