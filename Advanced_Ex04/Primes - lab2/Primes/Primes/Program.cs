19f4efd17e449de46708e16c884aa5006b0a97e1
orel <orele1995@gmail.com>
2016-08-04 15:58:45 +0300

    change

--- Advanced_Ex04/Primes - lab2/Primes/Primes/Program.cs
+++ Advanced_Ex04/Primes - lab2/Primes/Primes/Program.cs
@@ -11,30 +11,35 @@ namespace Primes
     {
         static void Main(string[] args)
         {
-            var watch = Stopwatch.StartNew();
-            Console.WriteLine(CalcPrimes(1, 500, 2).Count);
-            Console.WriteLine(watch.Elapsed);
-            watch.Restart();
-            Console.WriteLine(CalcPrimes(1, 500, 3).Count);
-            Console.WriteLine(watch.Elapsed);
-            watch.Restart();
-            Console.WriteLine(CalcPrimes(1, 500, 4).Count);
-            Console.WriteLine(watch.Elapsed);
+           
+            Console.WriteLine(CalcPrimes(1, 30000000).Count);
+            Console.WriteLine(CalcPrimes(1, 30000000).Count);
+            Console.WriteLine(CalcPrimes(1, 30000000).Count);
+            Console.WriteLine(CalcPrimes(1, 30000000).Count);
+       
+            
+            Console.ReadLine();
+            
         }
 
-        public static List<int> CalcPrimes(int start, int end, int maxDegreeOfParallelism)
+
+        public static List<int> CalcPrimes(int start, int end)
         {
-            ParallelOptions pOptions = new ParallelOptions();
-            pOptions.MaxDegreeOfParallelism = maxDegreeOfParallelism;
+            Random random = new Random();
 
             if (start < 2) { start = 2; } // 2 is the first prime number
             List<int> prime_numbers = new List<int>();
 
-            Parallel.For(start, end, pOptions, i =>
+            Parallel.For(start, end, (i,state) =>
             {
+                if (random.Next(10000000) == 0)
+                {
+                    state.Stop();
+                    return;
+                }
                 if (isPrime(i))
                     prime_numbers.Add(i);
-            } );
+            });
 
             return prime_numbers;
         }
