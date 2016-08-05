6eee1667f0ef0b1438e9f5ff7a0243559014b8f0
orel <orele1995@gmail.com>
2016-08-05 15:54:24 +0300

    add

---
+++ Advanced_Ex04/Lab5/Lab5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+           Console.WriteLine(watch.Elapsed);
+            waitTimeAsync(3000, watch);
+            watch.Restart();
+            waitTimeAsync(1000, watch);
+            Console.ReadLine();
+        }
+
+        static async void waitTimeAsync(int milliseconds, Stopwatch watch)
+        {
+            await milliseconds;
+            Console.WriteLine(watch.Elapsed);
+        }
+
+    }
+}
